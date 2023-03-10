ef9577d221dddb3e8138da6bdf50ce802c7fc9d5
Ulaş Öztürk <98660610+ulasozturkk@users.noreply.github.com>
2023-03-11 00:06:40 +0200

    add: Firebase options and LogIn page

--- lib/main.dart
+++ lib/main.dart
@@ -11,105 +11,117 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+        
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const myLoginPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class myLoginPage extends StatelessWidget{
+  const myLoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+
+      backgroundColor: Colors.lightGreen,
+
+      body: Padding(
+        padding:  EdgeInsets.symmetric(horizontal: screenWidth * 0.05),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              customText(text: "Welcome",fontsize: 40,color: Colors.white,),
+              customSizedBox(screenHeight * 0.01),
+              customText(text: "Log In to Continue", fontsize: 24,color: Colors.white,),
+              customSizedBox(screenHeight * 0.01),
+              TextFormField(
+                decoration: CustomInputDecoration("Username"),
+              ),
+              customSizedBox(screenHeight * 0.01),
+              TextFormField(
+                decoration: CustomInputDecoration("Password"),
+              ),
+              customSizedBox(screenHeight * 0.02),
+              TextButton(onPressed: () {}, child: customText(text: "forgot password?", fontsize: 16,color: Colors.black,)),
+              customSizedBox(screenHeight * 0.07),
+              customButton(screenHeight: screenHeight, screenWidth: screenWidth),
+              customSizedBox(screenHeight * 0.20),
+              TextButton(onPressed: () {}, child: customText(text: "forgot password?", fontsize: 20,color: Colors.black,)),
+
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  InputDecoration CustomInputDecoration(String hinttext) {
+    return InputDecoration(
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide( color:  Colors.white)
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black)
+                ),
+                hintText: hinttext,
+                hintStyle: TextStyle(color: Colors.white)
+              );
+  }
+
+
+}
+
+class customButton extends StatelessWidget {
+  const customButton({
+    super.key,
+    required this.screenHeight,
+    required this.screenWidth,
+  });
+
+  final double screenHeight;
+  final double screenWidth;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      child: Container(
+        height: screenHeight * 0.07,
+        width: screenWidth * 0.7,
+        decoration: BoxDecoration(
+          color: Colors.black,
+          borderRadius: BorderRadius.circular(20)
+        ),
+        child: Center(child: customText(text: "Log In", fontsize: 24, color: Colors.white)),
+      ),
+      onTap: (){},
+    );
+  }
+}
+
+Widget customSizedBox(double height) => SizedBox(height: height);
+class customText extends StatelessWidget {
+  const customText({
+    super.key,
+    required this.text,
+    required this.fontsize, required this.color
+  });
+
+
+  final String text;
+  final double fontsize;
+  final Color color;
+  @override
+  Widget build(BuildContext context) {
+    return Text(text, style: TextStyle(fontSize: fontsize,color: color),);
   }
 }
